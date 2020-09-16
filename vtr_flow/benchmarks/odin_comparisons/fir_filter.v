b9bf05e0484110efb72eaaf0c0a95dfc71d851d9
djns1 <djns1@students.waikato.ac.nz>
2020-09-16 12:36:51 +1200

    FIR unrol

--- vtr_flow/benchmarks/odin_comparisons/fir_filter.v
+++ vtr_flow/benchmarks/odin_comparisons/fir_filter.v
@@ -126,102 +126,6 @@ endmodule
 //Fir_Filter Behavioral Model
 //Jonathan Allen
 //ECE 659 HW#1
-/*module fir_filter(
-	clk,
-	reset,
-	data_in,
-	data_out,
-	coef_in,
-	load_c
-);
-//Input Ports
-input clk;
-input reset;
-input [7:0] data_in;
-input [7:0] coef_in;
-input load_c;
-
-//Output Ports
-output [17:0] data_out;
-
-//Data Type Declarations for IO
-wire clk;
-wire reset;
-wire [7:0] data_in;
-wire [7:0] coef_in;
-wire load_c;
-reg [17:0] temp;
-
-reg [17:0] data_out;
-
-//Other Declarations
-reg [7:0] r_storage [24:0];
-reg [7:0] coef_storage [24:0];
-reg[31:0] i;
-reg [15:0] product_array [24:0];
-
-lpm_mult0 m0(r_storage[0],coef_storage[0],clk,product_array[0]);
-lpm_mult0 m1(r_storage[1],coef_storage[1],clk,product_array[1]);
-lpm_mult0 m2(r_storage[2],coef_storage[2],clk,product_array[2]);
-lpm_mult0 m3(r_storage[3],coef_storage[3],clk,product_array[3]);
-lpm_mult0 m4(r_storage[4],coef_storage[4],clk,product_array[4]);
-lpm_mult0 m5(r_storage[5],coef_storage[5],clk,product_array[5]);
-lpm_mult0 m6(r_storage[6],coef_storage[6],clk,product_array[6]);
-lpm_mult0 m7(r_storage[7],coef_storage[7],clk,product_array[7]);
-lpm_mult0 m8(r_storage[8],coef_storage[8],clk,product_array[8]);
-lpm_mult0 m9(r_storage[9],coef_storage[9],clk,product_array[9]);
-lpm_mult0 m10(r_storage[10],coef_storage[10],clk,product_array[10]);
-lpm_mult0 m11(r_storage[11],coef_storage[11],clk,product_array[11]);
-lpm_mult0 m12(r_storage[12],coef_storage[12],clk,product_array[12]);
-lpm_mult0 m13(r_storage[13],coef_storage[13],clk,product_array[13]);
-lpm_mult0 m14(r_storage[14],coef_storage[14],clk,product_array[14]);
-lpm_mult0 m15(r_storage[15],coef_storage[15],clk,product_array[15]);
-lpm_mult0 m16(r_storage[16],coef_storage[16],clk,product_array[16]);
-lpm_mult0 m17(r_storage[17],coef_storage[17],clk,product_array[17]);
-lpm_mult0 m18(r_storage[18],coef_storage[18],clk,product_array[18]);
-lpm_mult0 m19(r_storage[19],coef_storage[19],clk,product_array[19]);
-lpm_mult0 m20(r_storage[20],coef_storage[20],clk,product_array[20]);
-lpm_mult0 m21(r_storage[21],coef_storage[21],clk,product_array[21]);
-lpm_mult0 m22(r_storage[22],coef_storage[22],clk,product_array[22]);
-lpm_mult0 m23(r_storage[23],coef_storage[23],clk,product_array[23]);
-lpm_mult0 m24(r_storage[24],coef_storage[24],clk,product_array[24]);
-
-//par_add0 parallel_add(product_array[0],product_array[1],product_array[2],product_array[3],data_out);
-
-
-//Code starts here
-always @ (posedge clk)
-begin
-
-	if(reset==1'b1)begin		//reset
-
-		for(i=0;i<25;i=i+1)begin
-			r_storage[i]<= 8'b00000000;
-			coef_storage[i]<=8'b00000000;
-		end
-	end
-	else if(load_c==1'b1)begin	//load coeficients
-		for(i=24;i>0;i=i-1)begin
-			coef_storage[i] = coef_storage[i-1];
-		end
-		coef_storage[0]=coef_in;
-	end
-	else begin
-		for(i=24;i>0;i=i-1)begin
-			r_storage[i] = r_storage[i-1];
-		end
-		r_storage[0]=data_in;
-		temp=0;
-		for(i=0;i<25;i=i+1)begin
-			temp=temp+product_array[i];
-		end
-		data_out=temp;
-	end
-end
-endmodule
-
-*/
-
 module fir_filter(
 	clk,
 	reset,
@@ -246,41 +150,89 @@ wire reset;
 wire [7:0] data_in;
 wire [7:0] coef_in;
 wire load_c;
-reg [17:0] temp;
 
 reg [17:0] data_out;
 
 //Other Declarations
 reg [7:0] r_storage [24:0];
-reg [7:0] coef_storage [24:0];
-reg[4:0] i;
+reg [7:0] r_storage0;
+reg [7:0] r_storage1;
+reg [7:0] r_storage2;
+reg [7:0] r_storage3;
+reg [7:0] r_storage4;
+reg [7:0] r_storage5;
+reg [7:0] r_storage6;
+reg [7:0] r_storage7;
+reg [7:0] r_storage8;
+reg [7:0] r_storage9;
+reg [7:0] r_storage10;
+reg [7:0] r_storage11;
+reg [7:0] r_storage12;
+reg [7:0] r_storage13;
+reg [7:0] r_storage14;
+reg [7:0] r_storage15;
+reg [7:0] r_storage16;
+reg [7:0] r_storage17;
+reg [7:0] r_storage18;
+reg [7:0] r_storage19;
+reg [7:0] r_storage20;
+reg [7:0] r_storage21;
+reg [7:0] r_storage22;
+reg [7:0] r_storage23;
+reg [7:0] r_storage24;
+reg [7:0] coef_storage0;
+reg [7:0] coef_storage1;
+reg [7:0] coef_storage2;
+reg [7:0] coef_storage3;
+reg [7:0] coef_storage4;
+reg [7:0] coef_storage5;
+reg [7:0] coef_storage6;
+reg [7:0] coef_storage7;
+reg [7:0] coef_storage8;
+reg [7:0] coef_storage9;
+reg [7:0] coef_storage10;
+reg [7:0] coef_storage11;
+reg [7:0] coef_storage12;
+reg [7:0] coef_storage13;
+reg [7:0] coef_storage14;
+reg [7:0] coef_storage15;
+reg [7:0] coef_storage16;
+reg [7:0] coef_storage17;
+reg [7:0] coef_storage18;
+reg [7:0] coef_storage19;
+reg [7:0] coef_storage20;
+reg [7:0] coef_storage21;
+reg [7:0] coef_storage22;
+reg [7:0] coef_storage23;
+reg [7:0] coef_storage24;
+reg[31:0] i;
 reg [15:0] product_array [24:0];
 
-lpm_mult0 m0(r_storage[0],coef_storage[0],clk,product_array[0]);
-lpm_mult0 m1(r_storage[1],coef_storage[1],clk,product_array[1]);
-lpm_mult0 m2(r_storage[2],coef_storage[2],clk,product_array[2]);
-lpm_mult0 m3(r_storage[3],coef_storage[3],clk,product_array[3]);
-lpm_mult0 m4(r_storage[4],coef_storage[4],clk,product_array[4]);
-lpm_mult0 m5(r_storage[5],coef_storage[5],clk,product_array[5]);
-lpm_mult0 m6(r_storage[6],coef_storage[6],clk,product_array[6]);
-lpm_mult0 m7(r_storage[7],coef_storage[7],clk,product_array[7]);
-lpm_mult0 m8(r_storage[8],coef_storage[8],clk,product_array[8]);
-lpm_mult0 m9(r_storage[9],coef_storage[9],clk,product_array[9]);
-lpm_mult0 m10(r_storage[10],coef_storage[10],clk,product_array[10]);
-lpm_mult0 m11(r_storage[11],coef_storage[11],clk,product_array[11]);
-lpm_mult0 m12(r_storage[12],coef_storage[12],clk,product_array[12]);
-lpm_mult0 m13(r_storage[13],coef_storage[13],clk,product_array[13]);
-lpm_mult0 m14(r_storage[14],coef_storage[14],clk,product_array[14]);
-lpm_mult0 m15(r_storage[15],coef_storage[15],clk,product_array[15]);
-lpm_mult0 m16(r_storage[16],coef_storage[16],clk,product_array[16]);
-lpm_mult0 m17(r_storage[17],coef_storage[17],clk,product_array[17]);
-lpm_mult0 m18(r_storage[18],coef_storage[18],clk,product_array[18]);
-lpm_mult0 m19(r_storage[19],coef_storage[19],clk,product_array[19]);
-lpm_mult0 m20(r_storage[20],coef_storage[20],clk,product_array[20]);
-lpm_mult0 m21(r_storage[21],coef_storage[21],clk,product_array[21]);
-lpm_mult0 m22(r_storage[22],coef_storage[22],clk,product_array[22]);
-lpm_mult0 m23(r_storage[23],coef_storage[23],clk,product_array[23]);
-lpm_mult0 m24(r_storage[24],coef_storage[24],clk,product_array[24]);
+lpm_mult0 m0(r_storage0,coef_storage0,clk,product_array[0]);
+lpm_mult0 m1(r_storage1,coef_storage1,clk,product_array[1]);
+lpm_mult0 m2(r_storage2,coef_storage2,clk,product_array[2]);
+lpm_mult0 m3(r_storage3,coef_storage3,clk,product_array[3]);
+lpm_mult0 m4(r_storage4,coef_storage4,clk,product_array[4]);
+lpm_mult0 m5(r_storage5,coef_storage5,clk,product_array[5]);
+lpm_mult0 m6(r_storage6,coef_storage6,clk,product_array[6]);
+lpm_mult0 m7(r_storage7,coef_storage7,clk,product_array[7]);
+lpm_mult0 m8(r_storage8,coef_storage8,clk,product_array[8]);
+lpm_mult0 m9(r_storage9,coef_storage9,clk,product_array[9]);
+lpm_mult0 m10(r_storage10,coef_storage10,clk,product_array[10]);
+lpm_mult0 m11(r_storage11,coef_storage11,clk,product_array[11]);
+lpm_mult0 m12(r_storage12,coef_storage12,clk,product_array[12]);
+lpm_mult0 m13(r_storage13,coef_storage13,clk,product_array[13]);
+lpm_mult0 m14(r_storage14,coef_storage14,clk,product_array[14]);
+lpm_mult0 m15(r_storage15,coef_storage15,clk,product_array[15]);
+lpm_mult0 m16(r_storage16,coef_storage16,clk,product_array[16]);
+lpm_mult0 m17(r_storage17,coef_storage17,clk,product_array[17]);
+lpm_mult0 m18(r_storage18,coef_storage18,clk,product_array[18]);
+lpm_mult0 m19(r_storage19,coef_storage19,clk,product_array[19]);
+lpm_mult0 m20(r_storage20,coef_storage20,clk,product_array[20]);
+lpm_mult0 m21(r_storage21,coef_storage21,clk,product_array[21]);
+lpm_mult0 m22(r_storage22,coef_storage22,clk,product_array[22]);
+lpm_mult0 m23(r_storage23,coef_storage23,clk,product_array[23]);
+lpm_mult0 m24(r_storage24,coef_storage24,clk,product_array[24]);
 
 //par_add0 parallel_add(product_array[0],product_array[1],product_array[2],product_array[3],data_out);
 
@@ -288,17 +240,134 @@ lpm_mult0 m24(r_storage[24],coef_storage[24],clk,product_array[24]);
 //Code starts here
 always @ (posedge clk)
 begin
-    for(i=24;i>0;i=i-1)begin
-        r_storage[i] = (reset) ? 8'b00000000 : (load_c) ? r_storage[i] : r_storage[i-1];
-        coef_storage[i] = (reset) ? 8'b00000000 : (load_c) ? coef_storage[i-1] : coef_storage[i];
-    end
-    r_storage[0] = (reset) ? 8'b00000000 : (load_c) ? r_storage[0] : data_in;
-    coef_storage[0] = (reset) ? 8'b00000000 : (load_c) ? coef_in : coef_storage[i];
 
-    temp=0;
-    for(i=0;i<25;i=i+1)begin
-        temp=temp+product_array[i];
-    end
-    data_out=(reset | load_c) ? data_out : temp;
+	if(reset==1'b1)begin		//reset
+        r_storage24 <= 8'b00000000;
+        r_storage23 <= 8'b00000000;
+        r_storage22 <= 8'b00000000;
+        r_storage21 <= 8'b00000000;
+        r_storage20 <= 8'b00000000;
+        r_storage19 <= 8'b00000000;
+        r_storage18 <= 8'b00000000;
+        r_storage17 <= 8'b00000000;
+        r_storage16 <= 8'b00000000;
+        r_storage15 <= 8'b00000000;
+        r_storage14 <= 8'b00000000;
+        r_storage13 <= 8'b00000000;
+        r_storage12 <= 8'b00000000;
+        r_storage11 <= 8'b00000000;
+        r_storage10 <= 8'b00000000;
+        r_storage9 <= 8'b00000000;
+        r_storage8 <= 8'b00000000;
+        r_storage7 <= 8'b00000000;
+        r_storage6 <= 8'b00000000;
+        r_storage5 <= 8'b00000000;
+        r_storage4 <= 8'b00000000;
+        r_storage3 <= 8'b00000000;
+        r_storage2 <= 8'b00000000;
+        r_storage1 <= 8'b00000000;
+        coef_storage24 <= 8'b00000000;
+        coef_storage23 <= 8'b00000000;
+        coef_storage22 <= 8'b00000000;
+        coef_storage21 <= 8'b00000000;
+        coef_storage20 <= 8'b00000000;
+        coef_storage19 <= 8'b00000000;
+        coef_storage18 <= 8'b00000000;
+        coef_storage17 <= 8'b00000000;
+        coef_storage16 <= 8'b00000000;
+        coef_storage15 <= 8'b00000000;
+        coef_storage14 <= 8'b00000000;
+        coef_storage13 <= 8'b00000000;
+        coef_storage12 <= 8'b00000000;
+        coef_storage11 <= 8'b00000000;
+        coef_storage10 <= 8'b00000000;
+        coef_storage9 <= 8'b00000000;
+        coef_storage8 <= 8'b00000000;
+        coef_storage7 <= 8'b00000000;
+        coef_storage6 <= 8'b00000000;
+        coef_storage5 <= 8'b00000000;
+        coef_storage4 <= 8'b00000000;
+        coef_storage3 <= 8'b00000000;
+        coef_storage2 <= 8'b00000000;
+        coef_storage1 <= 8'b00000000;
+	end
+	else if(load_c==1'b1)begin	//load coeficients
+		coef_storage24 = coef_storage23;
+		coef_storage23 = coef_storage22;
+		coef_storage22 = coef_storage21;
+		coef_storage21 = coef_storage20;
+		coef_storage20 = coef_storage19;
+		coef_storage19 = coef_storage18;
+		coef_storage18 = coef_storage17;
+		coef_storage17 = coef_storage16;
+		coef_storage16 = coef_storage15;
+		coef_storage15 = coef_storage14;
+		coef_storage14 = coef_storage13;
+		coef_storage13 = coef_storage12;
+		coef_storage12 = coef_storage11;
+		coef_storage11 = coef_storage10;
+		coef_storage10 = coef_storage9;
+		coef_storage9 = coef_storage8;
+		coef_storage8 = coef_storage7;
+		coef_storage7 = coef_storage6;
+		coef_storage6 = coef_storage5;
+		coef_storage5 = coef_storage4;
+		coef_storage4 = coef_storage3;
+		coef_storage3 = coef_storage2;
+		coef_storage2 = coef_storage1;
+		coef_storage1 = coef_storage0;
+		coef_storage0 = coef_in;
+	end
+	else begin
+        r_storage24 = r_storage23;
+        r_storage23 = r_storage22;
+        r_storage22 = r_storage21;
+        r_storage21 = r_storage20;
+        r_storage20 = r_storage19;
+        r_storage19 = r_storage18;
+        r_storage18 = r_storage17;
+        r_storage17 = r_storage16;
+        r_storage16 = r_storage15;
+        r_storage15 = r_storage14;
+        r_storage14 = r_storage13;
+        r_storage13 = r_storage12;
+        r_storage12 = r_storage11;
+        r_storage11 = r_storage10;
+        r_storage10 = r_storage9;
+        r_storage9 = r_storage8;
+        r_storage8 = r_storage7;
+        r_storage7 = r_storage6;
+        r_storage6 = r_storage5;
+        r_storage5 = r_storage4;
+        r_storage4 = r_storage3;
+        r_storage3 = r_storage2;
+        r_storage2 = r_storage1;
+        r_storage1 = r_storage0;
+        r_storage0 = data_in;
+		data_out=product_array[0]+
+                 		     product_array[1]+
+                 		     product_array[2]+
+                 		     product_array[3]+
+                 		     product_array[4]+
+                 		     product_array[5]+
+                 		     product_array[6]+
+                 		     product_array[7]+
+                 		     product_array[8]+
+                 		     product_array[9]+
+                 		     product_array[10]+
+                 		     product_array[11]+
+                 		     product_array[12]+
+                 		     product_array[13]+
+                 		     product_array[14]+
+                 		     product_array[15]+
+                 		     product_array[16]+
+                 		     product_array[17]+
+                 		     product_array[18]+
+                 		     product_array[19]+
+                 		     product_array[20]+
+                 		     product_array[21]+
+                 		     product_array[22]+
+                 		     product_array[23];
+	end
 end
 endmodule
