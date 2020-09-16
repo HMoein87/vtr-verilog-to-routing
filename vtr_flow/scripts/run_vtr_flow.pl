6f90942195112dd6c1b46e96bed583b519ea207b
djns1 <djns1@students.waikato.ac.nz>
2020-09-16 14:32:47 +1200

    CIN replace

--- vtr_flow/scripts/run_vtr_flow.pl
+++ vtr_flow/scripts/run_vtr_flow.pl
@@ -557,7 +557,7 @@ if ( $starting_stage <= $stage_idx_odin and !$error_code ) {
             $error_status = "failed: yosys";
             $error_code = 1;
         }else{
-			file_find_and_replace("$temp_dir$abc_output_file_name", "CARRY4", "CARRY4_VPR");
+			file_find_and_replace("$temp_dir$abc_output_file_name", "CARRY4 CI", "CARRY4_VPR CIN");
 		}
     } else {
         file_find_and_replace($odin_config_file_path, "XXX", $circuit_file_name);
