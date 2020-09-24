abc221572d8e9cecbba4f3834481d9f07e9af2ec
djns1 <djns1@students.waikato.ac.nz>
2020-09-25 09:52:54 +1200

    Fixy mc fix em up

--- vtr_flow/benchmarks/odin_comparisons/mcml.v
+++ vtr_flow/benchmarks/odin_comparisons/mcml.v
@@ -9951,6 +9951,57 @@ end
 
 endmodule
 
+module rng(clk, en, resetn,loadseed_i,seed_i,number_o);
+input clk;
+input resetn;
+input en;
+input loadseed_i;
+input [31:0] seed_i;
+output [31:0] number_o;
+
+reg [31:0] c_b1;
+reg [31:0] c_b2;
+reg [31:0] c_b3;
+reg [31:0] c_s1;
+reg [31:0] c_s2;
+reg [31:0] c_s3;
+
+reg [95:0] r;
+
+wire [31:0] number_o;
+
+assign number_o = r[31:0] ^ r[63:32] ^ r[95:64];
+
+always @(posedge clk or negedge resetn)
+begin
+     r <= (!resetn) ? 96'h0 :
+          (en) ? {c_s3, c_s2, c_s1} :
+          r;
+end
+
+always @(loadseed_i or seed_i or r)
+begin
+	if(loadseed_i)
+	begin
+		c_b1 = 32'b0;
+		c_s1 = seed_i;
+		c_b2 = 32'b0;
+		c_s2 = {seed_i[5:0], seed_i[17], seed_i[18], seed_i[19], seed_i[20], seed_i[25:21], seed_i[31:26], seed_i[16:6]} ^ 32'd1493609598;
+		c_b3 = 32'b0;
+		c_s3 = {seed_i[23:16], seed_i[5], seed_i[6], seed_i[7], seed_i[15:8], seed_i[4:0], seed_i[31:24]} ^ 32'd3447127471;
+	end
+	else
+	begin
+		c_b1 = (((r[31:0] << 13) ^ r[31:0]) >> 19);
+		c_s1 = (((r[31:0] & 32'd4294967294) << 12) ^ c_b1);
+		c_b2 = (((r[63:32] << 2) ^ r[63:32]) >> 25);
+		c_s2 = (((r[63:32] & 32'd4294967288) << 4) ^ c_b2);
+		c_b3 = (((r[95:64] << 3) ^ r[95:64]) >> 11);
+		c_s3 = (((r[95:64] & 32'd4294967280) << 17) ^ c_b3);
+	end
+end
+endmodule
+
 
 /////////////////////////////////////////////////////////////
 //mult_signed_32_bc
@@ -10467,59 +10518,6 @@ always @ (posedge clock) begin
 end
 
 endmodule
-
-
-module rng(clk, en, resetn,loadseed_i,seed_i,number_o);
-input clk;
-input resetn;
-input en;
-input loadseed_i;
-input [31:0] seed_i;
-output [31:0] number_o;
-
-reg [31:0] c_b1;
-reg [31:0] c_b2;
-reg [31:0] c_b3;
-reg [31:0] c_s1;
-reg [31:0] c_s2;
-reg [31:0] c_s3;
-
-reg [95:0] r;
-
-wire [31:0] number_o;
-
-assign number_o = r[31:0] ^ r[63:32] ^ r[95:64];
-
-always @(posedge clk or negedge resetn)
-begin
-     r <= (!resetn) ? 96'h0 :
-          (en) ? {c_s3, c_s2, c_s1} :
-          r;
-end
-
-always @(loadseed_i or seed_i or r)
-begin
-	if(loadseed_i)
-	begin
-		c_b1 = 32'b0;
-		c_s1 = seed_i;
-		c_b2 = 32'b0;
-		c_s2 = {seed_i[5:0], seed_i[17], seed_i[18], seed_i[19], seed_i[20], seed_i[25:21], seed_i[31:26], seed_i[16:6]} ^ 32'd1493609598;
-		c_b3 = 32'b0;
-		c_s3 = {seed_i[23:16], seed_i[5], seed_i[6], seed_i[7], seed_i[15:8], seed_i[4:0], seed_i[31:24]} ^ 32'd3447127471;
-	end
-	else
-	begin
-		c_b1 = (((r[31:0] << 13) ^ r[31:0]) >> 19);
-		c_s1 = (((r[31:0] & 32'd4294967294) << 12) ^ c_b1);
-		c_b2 = (((r[63:32] << 2) ^ r[63:32]) >> 25);
-		c_s2 = (((r[63:32] & 32'd4294967288) << 4) ^ c_b2);
-		c_b3 = (((r[95:64] << 3) ^ r[95:64]) >> 11);
-		c_s3 = (((r[95:64] & 32'd4294967280) << 17) ^ c_b3);
-	end
-end
-endmodule
-
 
 
 module LogCalc(clock, reset, enable, in_x, log_x);
